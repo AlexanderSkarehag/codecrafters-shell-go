85bc554a0d1d46e61f667084cad22ea4a69165ed
AlexanderSkarehag <Aleeex.S@gmail.com>
2025-01-31 21:48:00 +0100

    Check stuff

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,6 +11,21 @@ import (
 var _ = fmt.Fprint
 
 func checkIfBuiltin(t string) {
+	path := os.Getenv("PATH")
+	if path != "" {
+		fmt.Println("PATH is :" + path)
+	}
+	paths := strings.Split(path, ":")
+	s := ""
+	if(len(paths)>0) {
+		for i := 0; i < len(paths) i++ {
+			r, err := os.Stat(paths[i])
+			if(err != nil) {
+				fmt.Println("Error occurred: "+ err)	
+			}
+			fmt.Println(r)
+		}
+	}
 	switch t {
 	case "echo":
 		fmt.Println(t + " is a shell builtin")
@@ -38,22 +53,45 @@ loop:
 
 		command = strings.TrimSpace(command)
 
-		switch {
-		case command == "exit 0":
-			os.Exit(0)
-			break loop
-		case strings.HasPrefix(command, "echo"):
-			after, hasPrefix := strings.CutPrefix(command, "echo")
-			if hasPrefix {
-				fmt.Println(strings.TrimSpace(after))
+		paths := strings.SplitN(command, " ", 2)
+
+		cmd := paths[0]
+		args := ""
+		if len(paths) > 1 {
+			args = paths[1]
+		}
+
+		switch cmd {
+		case "exit":
+			if args == "0" {
+				os.Exit(0)
+				break loop
 			}
-		case strings.HasPrefix(command, "type"):
-			after, hasFound := strings.CutPrefix(command, "type")
-			if hasFound {
-				checkIfBuiltin(strings.TrimSpace(after))
-			}
+		case "echo":
+			fmt.Println(strings.TrimSpace(args))
+		case "type":
+			checkIfBuiltin(strings.TrimSpace(args))
 		default:
 			fmt.Println(command + ": command not found")
 		}
+		/*
+			switch {
+			case command == "exit 0":
+				os.Exit(0)
+				break loop
+			case strings.HasPrefix(command, "echo"):
+				after, hasPrefix := strings.CutPrefix(command, "echo")
+				if hasPrefix {
+					fmt.Println(strings.TrimSpace(after))
+				}
+			case strings.HasPrefix(command, "type"):
+				after, hasFound := strings.CutPrefix(command, "type")
+				if hasFound {
+					checkIfBuiltin(strings.TrimSpace(after))
+				}
+			default:
+				fmt.Println(command + ": command not found")
+			}
+		*/
 	}
 }
